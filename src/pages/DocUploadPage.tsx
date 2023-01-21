f4183185cfef913eca2b8df4bf3442ef20c34783
someboiroy <roywashington1547@gmail.com>
2023-01-21 15:37:50 -0600

    basic page structure

---
+++ src/pages/DocUploadPage.tsx
@@ -0,0 +1,15 @@
+export default function DocUploadPage(){
+    return(
+        <main>
+            <div className="hero min-h-screen bg-secondary-400">
+            <div className="hero-content text-left ">
+                <div className="max-w-md">
+                <h1 className="text-4xl font-serif">Please upload a syllabus for bias detection...</h1>
+                <input type="file" className="mt-4 file-input file-input-bordered w-full file-input-primary max-w-xs" />
+            </div>
+            </div>
+            </div>
+            <h1></h1>
+        </main>
+    )
+}
