e0f385cc469385e43a75eded1a28c6aa11d9e5c3
someboiroy <roywashington1547@gmail.com>
2023-01-21 18:35:14 -0600

    submit button added

--- src/pages/DocUploadPage.tsx
+++ src/pages/DocUploadPage.tsx
@@ -24,7 +24,10 @@ export default function DocUploadPage(){
             <div className="hero-content text-left ">
                 <div className="max-w-md">
                 <h1 className="text-4xl font-serif">Please upload a syllabus for bias detection...</h1>
+                <div className="grid place-items-center">
                 <input type="file" className="mt-4 file-input file-input-bordered w-full file-input-primary max-w-xs" />
+                <button type="submit" className="btn btn-block mt-2">Submit</button>
+                </div>
             </div>
             </div>
             </div>
