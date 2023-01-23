92be46f911e0709661fce68955483f221a12f3e9
someboiroy <roywashington1547@gmail.com>
2023-01-23 09:02:29 -0600

    initial setup

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-
 import { prisma } from "../../../server/db";
 
 export const authOptions: NextAuthOptions = {
