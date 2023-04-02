6b1e1f139dfd87083356499c5484e9364abb160d
Roy <roywashington1547@gmail.com>
2023-04-02 17:10:19 -0500

    added ability to accept  .pdf files

--- apps/api/semtatic-search-api/main.py
+++ apps/api/semtatic-search-api/main.py
@@ -1,7 +1,9 @@
+import os
 from semanticSearch import semanticSearch as Search 
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from PyPDF2 import PdfFileReader
 
 class Syllabus(BaseModel):
     name: str
@@ -9,6 +11,20 @@ class Syllabus(BaseModel):
     corpus: str
     biasScore: float
     findings: list
+    
+    
+def checkExtension(file):
+    return os.path.splitext(file)[1]
+                        
+def getPDFText(file):
+    alltext = ""
+    reader = PdfFileReader(file)
+    numOfPages = len(reader.pages)
+    for i in range(numOfPages):
+        page = reader.getPage(i)
+        alltext += page.extractText()
+    return alltext
+        
     
 
 app = FastAPI()
@@ -37,10 +53,16 @@ def read_root():
 
 
 @app.post("/upload")
-async def process_syllabus(syllabus_file: UploadFile = File(...), owner_ID: str = None ):
-    syllabusText = await syllabus_file.read()
+async def process_syllabus(syllabus_file: UploadFile = File(...), owner_ID: str = ""):
+    extension = checkExtension(syllabus_file.filename)
     
-    Findings = Search(syllabusText.decode('utf-8'))
+    if extension == ".pdf":
+        syllabusText = getPDFText(syllabus_file.file)
+    else:
+        syllabusText = await syllabus_file.read()
+        syllabusText = syllabusText.decode('utf-8')
+    
+    Findings = Search(syllabusText)
     Syllabus_ForDB = Syllabus(name = syllabus_file.filename, ownerID = owner_ID, corpus = Findings.corpus, biasScore = Findings.docScore, findings = Findings.findings)
         
     return Syllabus_ForDB
