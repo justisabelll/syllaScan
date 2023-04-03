7573d8a4cb8a66c5d8f04774a63b3be1af17b158
Roy <roywashington1547@gmail.com>
2023-04-03 11:26:56 -0500

    uploads syllabi to db on upload now

--- apps/api/semtatic-search-api/main.py
+++ apps/api/semtatic-search-api/main.py
@@ -1,17 +1,13 @@
 import os
+import docx
+import tempfile
 from semanticSearch import semanticSearch as Search 
-from fastapi import FastAPI, File, UploadFile
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfFileReader
+from db_upload import uploadtoDB, Syllabus
 
-class Syllabus(BaseModel):
-    name: str
-    ownerID: str
-    corpus: str
-    biasScore: float
-    findings: list
-    
+
     
 def checkExtension(file):
     return os.path.splitext(file)[1]
@@ -24,7 +20,17 @@ def getPDFText(file):
         page = reader.getPage(i)
         alltext += page.extractText()
     return alltext
-        
+
+def getDocxText(file):
+    alltext = ""
+    with tempfile.NamedTemporaryFile(delete=True) as tmp:
+        tmp.write(file.read())
+        tmp.flush()
+        document = docx.Document(tmp.name)
+        for para in document.paragraphs:
+            alltext += para.text
+    return alltext
+
     
 
 app = FastAPI()
@@ -54,16 +60,21 @@ def read_root():
 
 @app.post("/upload")
 async def process_syllabus(syllabus_file: UploadFile = File(...), owner_ID: str = ""):
+    
     extension = checkExtension(syllabus_file.filename)
     
     if extension == ".pdf":
         syllabusText = getPDFText(syllabus_file.file)
-    else:
+    elif extension == ".docx":
+        syllabusText = getDocxText(syllabus_file.file)
+    elif extension == ".txt":
         syllabusText = await syllabus_file.read()
         syllabusText = syllabusText.decode('utf-8')
-    
+    else:
+        raise HTTPException(status_code=400, detail="Unsupported file type, Please upload a .pdf, .txt, or .docx")
+        
     Findings = Search(syllabusText)
     Syllabus_ForDB = Syllabus(name = syllabus_file.filename, ownerID = owner_ID, corpus = Findings.corpus, biasScore = Findings.docScore, findings = Findings.findings)
-        
-    return Syllabus_ForDB
+    
+    return uploadtoDB(Syllabus_ForDB)
 
